36a98f6b7c828ab2c8504b60ab019a2d18fe0993
3abdeldaayem <54505330+3abdeldaayem@users.noreply.github.com>
2025-11-26 14:05:47 +0200

    init project.

---
+++ lib/HomeScree.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  static const string routName='Home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Quran')
+      ),
+      
+      ),
+
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(app);
+}
+class MyMyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: 
+      routes: {
+        
+      },
+    );
+  }
+}
